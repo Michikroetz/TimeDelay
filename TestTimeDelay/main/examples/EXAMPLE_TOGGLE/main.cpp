51674dff5f0118a34bc32f5f912d15a9cd71b31e
Michael Krötz <l0622@schueler.ms-muc.de>
2022-02-22 10:19:27 +0100

    Uebung Toogle 1 Q1Q2

--- TestTimeDelay/main/examples/EXAMPLE_TOGGLE/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TOGGLE/main.cpp
@@ -44,17 +44,20 @@ extern "C" void app_main(void)
     */
     gpio_reset_pin(GPIO_Q1);
     gpio_reset_pin(GPIO_Q2);
+    gpio_reset_pin(GPIO_Q3);
     gpio_reset_pin(BUTTON_I1);
     gpio_reset_pin(BUTTON_I2);
     gpio_reset_pin(BUTTON_I3);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_Q2, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_Q3, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
     gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
     gpio_set_direction(BUTTON_I3, GPIO_MODE_INPUT);
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
     gpio_set_level(GPIO_Q2, 0); //set to 0 at Reset.
+    gpio_set_level(GPIO_Q3, 0); //set to 0 at Reset.
 
 
     TOGGLE TOGGLE1;
@@ -79,6 +82,7 @@ extern "C" void app_main(void)
         // Ausgaenge setzen
         gpio_set_level(GPIO_Q1, TOGGLE1.Q);
         gpio_set_level(GPIO_Q2, TOGGLE2.Q);
+        gpio_set_level(GPIO_Q3, TOGGLE1.Q);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
